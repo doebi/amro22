bef32fe682a324cfb1f766fbf9e10fea6d173179
doebi <christoph@doeberl.at>
2022-06-11 19:09:49 +0200

    added mqtt

---
+++ js/client.js
@@ -0,0 +1,48 @@
+let id = window.crypto.getRandomValues(new Uint32Array(1))[0].toString(16);
+var client = new Paho.Client('test.mosquitto.org', 8080, 'web-' + id);
+
+let topic = "amro/playerone/stream";
+
+client.onMessageArrived = onMessageArrived;
+
+client.connect({ onSuccess, reconnect: true });
+
+function log(obj) {
+  let el = document.createElement('div');
+  el.textContent = obj;
+  feed.appendChild(el);
+}
+
+function onSuccess() {
+  client.subscribe('amro/#');
+}
+
+function onMessageArrived(message) {
+  console.log(message);
+  /*
+  let cmd = message.topic.split('/').pop();
+  switch(cmd) {
+    case 'stream':
+      log(message.payloadString)
+      break;
+    default:
+      break;
+  }
+  */
+}
+
+function send(payload) {
+  if (client.isConnected) {
+    client.publish(topic, JSON.stringify(payload));
+  }
+}
+
+window.addEventListener('pointerdown', function({ x, y }) {
+  let payload = { x, y };
+  send(payload);
+});
+
+window.addEventListener('deviceorientation', function(event) {
+  let payload = { alpha, beta, gamma };
+  send(payload);
+});
